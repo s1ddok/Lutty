4105e4cf60b1f74dddf5ec9ff31f8db5a336c9d0
s1ddok <siddok@gmail.com>
2019-10-30 14:26:03 +0300

    introduce filter model

---
+++ Lutty/Model/Filter.swift
@@ -0,0 +1,14 @@
+//
+//  Filter.swift
+//  Lutty
+//
+//  Created by Andrey Volodin on 29.10.2019.
+//  Copyright © 2019 Andrey Volodin. All rights reserved.
+//
+
+import Foundation
+
+public struct Filter: Identifiable {
+    public let id: Int
+    public let data: Data
+}
